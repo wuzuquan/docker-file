1a733bb3eef81d0987e8e74040e5d096c0e5e0e3
wuzuquan <403621433@qq.com>
2018-08-20 09:26:36 +0800

    redis：优化docker内核

--- redis/set-redis-pass.sh
+++ redis/set-redis-pass.sh
@@ -8,4 +8,17 @@ if [ "$REDISPASSWORD" != "NULL" ]; then
 
 
 fi
+#disable THP
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+
+#sysctl.conf
+echo vm.max_map_count=65538 >> /etc/sysctl.conf
+echo vm.overcommit_memory=1 >> /etc/sysctl.conf
+echo net.core.somaxconn= 16384 >> /etc/sysctl.conf
+
+
+#sysctl -w net.core.netdev_max_backlog=16384
+#sysctl -w net.ipv4.tcp_max_syn_backlog=16384
+sysctl -p
+
 exec "$@"
