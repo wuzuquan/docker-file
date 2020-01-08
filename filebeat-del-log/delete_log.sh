546e6be78618d1718d292f06919cbf8d5d1f5da9
吴祖全 <wuzuquan@huafagroup.com>
2020-01-08 12:01:27 +0800

    更新自动删除日志daemonset

--- filebeat-del-log/delete_log.sh
+++ filebeat-del-log/delete_log.sh
@@ -4,5 +4,20 @@ while true;do
 echo `date "+%Y-%m-%d %H:%M:%S"`" start delete log."
 #find /var/applog -mtime +$LOG_RETAIN_DAYS -type f -name "*.log" | xargs rm -f
 find /var/applog -mtime +$LOG_RETAIN_DAYS -type f -regex .*\.log | xargs rm -f
+
+
+# clear  nginx log
+dir=/var/applog/nginx/
+i=0
+for file in $dir/*; do
+    let i+=1;
+    echo $file
+    #echo $i
+    #cp $file /var/log/nginx/log-$(date -d "yesterday" +"%Y-%m-%d")-$i.log
+    # clear .log
+    cat /dev/null > $file
+done
+
+
 sleep 72000
 done
